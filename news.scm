11c2adc22b71409d7a257dcb93bbfe6724fa04ff
David Larsson <david.larsson@selfhosted.xyz>
2020-06-04 13:33:19 +0200

    adding channel and channel news files

---
+++ news.scm
@@ -0,0 +1,6 @@
+(channel-news
+ (version 0)
+ (entry (commit "b5a30661119fd7111abcaba457e71b6873ec939e")
+        (title (en "Channel packages")
+        (body
+         (en "Here are some python and emacs packages not found in standard guix. Additional packages may be published here but feel free to add them to standard Guix. If you do, please open an issue and ask me to remove or comment out those packages from this repo")
